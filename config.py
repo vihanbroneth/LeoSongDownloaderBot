564c00b0dab1774f3c8f55848ba453cba2438b95
vihanbroneth <100992172+vihanbroneth@users.noreply.github.com>
2022-03-06 21:39:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Leo Projects <https://t.me/leosupportx>
 
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("13103999"))
+API_HASH = os.getenv("a5f922cbb1eb2c046c8ef106d6545358")
+BOT_TOKEN = os.getenv("5257184670:AAGIymK9UMWc6GYI6mPTmgeZUuyw6TQmI5I")
